742b736c5b240488f1801b48ca841bef28357dc9
pangpyo <kkp0639@gmail.com>
2022-08-05 16:47:55 +0900

    0805 2회차 김광표 모의고사

--- 2회차/김광표/0_민석이의과제체크하기.py
+++ 2회차/김광표/0_민석이의과제체크하기.py
@@ -1,3 +1,15 @@
+# 5431 민석이의 과제 체크하기 D3
+
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T = int(input())
+
+for i in range(1, T+1) : 
+    N, K = map(int, input().split()) 
+    stu = list(range(1, N+1)) # 모든 학생들의 리스트를 번호로 만든다.
+    stu_ass = map(int, input().split()) # 숙제를 제출한 학생을 입력받는다.
+    for j in stu_ass : # 모든 학생들의 리스트에서 숙제를 제출한 학생의 번호를 지워준다.
+        stu.remove(j)
+    print('#%d'%i, *stu)
--- 2회차/김광표/2_암호생성기.py
+++ 2회차/김광표/2_암호생성기.py
@@ -1,3 +1,25 @@
+# 1225 암호생성기 D3
+
 import sys
 
 sys.stdin = open("_암호생성기.txt")
+
+while 1 :
+    try :
+        i = int(input())
+    except :
+        break
+    password = list(map(int, input().split()))
+    point = 0 # break 포인트 설정
+    while 1 : 
+        for j in range(1, 6) : #1~5까지의 범위 설정
+            a = password.pop(0) # 첫번째 인덱스를 pop해줌
+            a -= j # pop 해준 값에 j를 빼줌(1~5)
+            password.append(a) # 다시 그 값을 리스트에 append해줌
+            if a <= 0 : # 그 과정에서 a가 0 이하가 되면 리스트의 마지막 값을 0으로 만든 뒤 브레이크해줌
+                password[-1] = 0
+                point = 1 # 바깥쪽의 while문도 break해주기 위한 변수
+                break
+        if point == 1 :
+            break
+    print('#%d'%i, *password)
