7bad28360ad3ff3dcfd9af093db5c4771541cad7
avocad06 <miggo2704@gmail.com>
2022-08-05 17:24:06 +0900

    1회차/안예지/모의고사/제출

--- 1회차/안예지/3_파리퇴치.py
+++ 1회차/안예지/3_파리퇴치.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+T = int(input())
+for t in range(1, T + 1):
+    N, M = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+    # print(matrix)
+    answer = []
+    for r in range(N - 1):
+        for c in range(N):
+            # 위쪽 행을 슬라이스한 결과
+            up = sum(matrix[r][c:c + M])
+            # 아래쪽 행을 슬라이스한 결과 
+            down = sum(matrix[r + 1][c:c + M])
+            answer.append(up+down)
+    print(max(answer))
+        
--- 1회차/안예지/4_어디에단어가들어갈수있을까.py
+++ 1회차/안예지/4_어디에단어가들어갈수있을까.py
@@ -1,3 +1,63 @@
 import sys
 
 sys.stdin = open("_어디에단어가들어갈수있을까.txt")
+"""
+0 0 1 1 1
+1 1 1 1 0
+0 0 1 0 0
+0 1 1 1 1
+1 1 1 0 1
+"""
+# matrix = [['0', '0', '1', '1', '1'],
+#         ['1', '1', '1', '1', '0'],
+#         ['0', '0', '1', '0', '0'],
+#         ['0', '1', '1', '1', '1'],
+#         ['1', '1', '1', '0', '1']]
+T = int(input())
+for t in range(1, T + 1):
+    N, K = map(int, input().split())
+    matrix = []
+    for r in range(N):
+        matrix.append(input().split())
+    from pprint import pprint
+    # pprint(matrix)
+    cnt = 0
+    word = 0
+    for r in range(N):
+        for c in range(N):
+            if matrix[r][c] == '0':
+                if cnt == K:
+                    word += 1
+                cnt = 0
+            else:
+                cnt += 1
+        if cnt == K:
+            word += 1
+        cnt = 0  
+    # transed_matrix = [['0', '1', '0', '0', '1'],
+    #                 ['0', '1', '0', '1', '1'],
+    #                 ['1', '1', '1', '1', '1'],
+    #                 ['1', '1', '0', '1', '0'],
+    #                 ['1', '0', '0', '1', '1']] 
+    transed_matrix = [[0] * len(matrix) for _ in range(len(matrix[0]))]
+    for c in range(len(matrix[0])):
+        for r in range(len(matrix)):
+            transed_matrix[c][r] = matrix[r][c]
+    # pprint(transed_matrix)
+
+    for r in range(len(transed_matrix)):
+        for c in range(len(transed_matrix[0])):
+            if transed_matrix[r][c] == '0':
+                if cnt == K:
+                    word += 1
+                cnt = 0
+        
+            else:
+                cnt += 1
+        if cnt == K:
+            word += 1
+        cnt = 0
+    print(f'#{t} {word}')
+    
+    
+    
