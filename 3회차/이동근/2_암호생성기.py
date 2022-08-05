c45cf2aadf93b7e417fafae3181a55b5e73c4068
qlghwp123 <qlghwp123@naver.com>
2022-08-05 15:35:44 +0900

    Complete PJT PS

--- 3회차/이동근/2_암호생성기.py
+++ 3회차/이동근/2_암호생성기.py
@@ -1,3 +1,15 @@
-import sys
+for _ in range(10):
+    T = int(input())
 
-sys.stdin = open("_암호생성기.txt")
+    line = list(map(int, input().split()))
+
+    # 복사본을 만들기 위해서 슬라이싱을 이용했다.
+    cycle = line[:]
+    i = 1
+    while cycle[-1] != 0:
+        val = 0 if cycle[0] - i < 1 else cycle[0] - i
+        cycle = cycle[1:]
+        cycle.append(val)
+        i = i + 1 if i < 5 else 1
+
+    print(f"#{T}", *cycle)
--- 3회차/이동근/4_어디에단어가들어갈수있을까.py
+++ 3회차/이동근/4_어디에단어가들어갈수있을까.py
@@ -1,3 +1,36 @@
-import sys
+T = int(input())
 
-sys.stdin = open("_어디에단어가들어갈수있을까.txt")
+for i in range(T):
+    N, K = map(int, input().split())
+
+    grid = [input().replace(' ', '') for _ in range(N)]
+    col = [''] * N
+    for j in range(N):
+        for k in range(N):
+            col[j] += grid[k][j]
+
+    count = 0
+    for j in range(N):
+        row_count = 0
+        col_count = 0
+
+        for k in range(N):
+            if grid[j][k] == '1':
+                row_count += 1
+            else:
+                if row_count == K:
+                    count += 1
+                row_count = 0
+            if col[j][k] == '1':
+                col_count += 1
+            else:
+                if col_count == K:
+                    count += 1
+                col_count = 0
+        
+        if row_count == K:
+            count += 1
+        if col_count == K:
+            count += 1
+
+    print(f"#{i + 1}", count)
