c316adced326a775b776d6d4caf97c8465fe90bc
w00ye0l <dnduf158@gmail.com>
2022-08-05 15:08:49 +0900

    1기 2회차 이우열 모의고사 주석 추가

--- 2회차/이우열/1_조교의성적매기기.py
+++ 2회차/이우열/1_조교의성적매기기.py
@@ -1,3 +1,35 @@
 import sys
 
 sys.stdin = open("_조교의성적매기기.txt")
+
+g = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+
+t = int(input())
+
+for i in range(1, t + 1):
+    n, k = map(int, input().split())
+    grade = []
+    score = {}
+    idx = 0
+
+    for j in g:
+        for l in range(n // 10):
+            # 학생의 수만큼 등급을 배로 늘려준다.
+            grade.append(j)
+
+    for j in range(1, n + 1):
+        mid, final, hw = map(int, input().split())
+        score[j] = (mid * 0.35) + (final * 0.45) + \
+            (hw * 0.2)   # 성적을 학생 수만큼 퍼센트를 계산하여 더해준다
+
+    # 성적을 기준으로 점수를 정렬한다
+    score = sorted(score.items(), key=lambda x: (-x[1], x[0]))
+
+    for j in range(n):
+        # 원하는 학생의 번호가 점수 튜플의 첫 번째 인덱스의 값과 같다면
+        if k == score[j][0]:
+            # 점수 튜플의 인덱스를 구하여 idx로 저장한다
+            idx = score.index(score[j])
+
+    # 등급 리스트에서 인덱스가 idx인 등급을 출력한다
+    print(f'#{i} {grade[idx]}')
--- 2회차/이우열/3_파리퇴치.py
+++ 2회차/이우열/3_파리퇴치.py
@@ -1,3 +1,28 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+t = int(input())
+
+for i in range(1, t + 1):
+    n, m = map(int, input().split())
+    fly = []
+
+    for j in range(n):
+        fly.append(list(map(int, input().split())))
+
+    # 잡을 수 있는 파리 수를 저장할 2차원 리스트 선언
+    # 2차원 리스트의 길이는 파리가 있는 리스트 길이에서 파리채의 길이를 뺀 후 1을 더한 길이다.
+    result = [[0] * (n - m + 1) for _ in range(n - m + 1)]
+
+    for j in range(n - m + 1):
+        for k in range(n - m + 1):              # 파리채가 이동 가능한 곳까지 반복 수행
+            sum_ = 0
+            for a in range(m):
+                for b in range(m):
+                    # 파리채의 길이만큼 행, 열로 이동하며 sum_에 파리수를 누적
+                    # ex) if m == 2: sum_ = fly[0][0] + fly[0][1] + fly[1][0] + fly[1][1]
+                    sum_ += fly[j + a][k + b]
+            result[j][k] = sum_                 # 잡을 수 있는 파리의 수를 2차원 리스트에 저장
+
+    print(f'#{i} {max(map(max, result))}')
