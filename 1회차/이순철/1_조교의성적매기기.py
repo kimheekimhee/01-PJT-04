c7ea317b6acd19b2fd6977db002e9c04d765a6e1
leesc8408 <leesc8408@gmail.com>
2022-08-05 17:30:49 +0900

    0805_모의고사2_프로젝트4

--- 1회차/이순철/1_조교의성적매기기.py
+++ 1회차/이순철/1_조교의성적매기기.py
@@ -1,3 +1,30 @@
 import sys
 
 sys.stdin = open("_조교의성적매기기.txt")
+
+# A+ A0 A- B+ B0 B- C+ C0 C- D0 평가 개수 10개  
+# 100 = m_score(35%) + e_score(45%) + h_work(20%)
+# 학생수 10<= n <= 100이고 10배수
+# 확인학생번호 1<= k <= n
+# 1줄:test_case / 2줄:n k / ~줄:m_score e_score h_work
+# 출력:#test_case 학점
+
+t = int(input())
+for test_case in range(1, t + 1):
+    n, k = map(int, input().split())
+    score_dic = {}
+    alpha_score = {}
+    alpha_ = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+
+    for i in range(1, n + 1):
+        m_score, e_score, h_work = map(int, input().split())
+        score_dic[i] = round((m_score*0.35)+(e_score*0.45)+(h_work*0.2))
+    
+    # print(score_dic)
+    k_score = score_dic[k]
+    # print(k_score)
+    all_score = list(score_dic.values())
+    all_score.sort(reverse=True)
+    cnt = all_score.index(k_score)
+    # print(cnt)
+    
--- 1회차/이순철/3_파리퇴치.py
+++ 1회차/이순철/3_파리퇴치.py
@@ -1,3 +1,19 @@
+from pprint import pprint
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+# 행렬값은 n*n 채 m*m
+# 행렬 칸 마다 숫자 있고 채에 해당하는 4칸의 숫자 합이 가장 큰 경우 출력
+# 5<=n<=15 /  2<=m<=n   /  칸의 숫자<=30
+
+t = int(input())
+for test_case in range(1, t + 1):
+    n, m = map(int, input().split())
+    pan = [list(map(int, input().split())) for _ in range(n)]
+    # pprint(pan)
+    
+    cnt = 0
+    for x in range(n - m):
+        for y in range(n - m):
+            
