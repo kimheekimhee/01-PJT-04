f3b29ca60c6b85876a00a100074ac12a3c0fe146
chaehyun_oh <ohchaehh@gmail.com>
2022-08-05 17:23:44 +0900

    1기 3회차 오채현

--- 3회차/오채현/0_민석이의과제체크하기.py
+++ 3회차/오채현/0_민석이의과제체크하기.py
@@ -1,3 +1,16 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T = int(input())
+
+for testcase in range(1, T + 1):
+    n, k = map(int, input().split())
+    student = list(range(1, n + 1))
+    sbm = list(map(int, input().split()))
+    mis = []
+    for s in student:
+        if s not in sbm:
+            mis.append(s)
+
+    print(f'#{testcase}', *mis)
