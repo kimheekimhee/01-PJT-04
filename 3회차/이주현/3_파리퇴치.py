1c1aabb1962c0187282e1d21758cd38ec2ba6fd3
rrwe23 <jhrrt0626@gmail.com>
2022-08-05 17:26:10 +0900

    0805 문제 풀이

--- 3회차/이주현/3_파리퇴치.py
+++ 3회차/이주현/3_파리퇴치.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+
+
+N = int(input())
+
+for i in range(N):
+    fly = list(map(int,input().split()))
+
+for i in range(N):
+    for j in range(N):
+        print(fly[i][j])
