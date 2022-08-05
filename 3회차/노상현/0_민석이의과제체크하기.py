3f5897a5b7c3ecfd536fb69497f2824c02e4e69e
rohsnaghyun <rohwinter@gmail.com>
2022-08-05 19:54:09 +0900

    0805 모의고사

--- 3회차/노상현/0_민석이의과제체크하기.py
+++ 3회차/노상현/0_민석이의과제체크하기.py
@@ -1,3 +1,15 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T =int(input())
+
+for tc in range(1, T+1):
+
+    a, b = map(int, input().split())
+    data = list(map(int,input().split()))
+    result = []
+
+    for i in range(1, a+1):
+        if i not in data:
+            result
