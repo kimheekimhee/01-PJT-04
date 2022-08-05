e5f0259a7543147db7310bb6fdc5dbdab1d8c95d
kfrrsi0301 <kfrrsi0301@naver.com>
2022-08-05 17:30:47 +0900

    2차모의고사 김푸르른솔이

--- 3회차/김푸르른솔이/4_어디에단어가들어갈수있을까.py
+++ 3회차/김푸르른솔이/4_어디에단어가들어갈수있을까.py
@@ -1,3 +1,27 @@
 import sys
 
 sys.stdin = open("_어디에단어가들어갈수있을까.txt")
+
+n = int(input())
+grid = []
+a, b = map(int, input().split())
+cnt = 0
+garo = 0
+for _ in range(1, n + 1):
+    for i in range(a):
+        line = list(input())
+        grid.append(line)
+    for i in range(a):
+        for j in range(a):
+            if grid[i][j] == '0':
+                cnt = 0
+            else:
+                cnt += 1
+                if cnt == b:
+                    garo += 1
+        cnt = 0
+
+print(cnt)
+
+    
+    
