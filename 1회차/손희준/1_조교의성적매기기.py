e3ae41e70fd06f96d28c6a0aa577ed53a67a7c7c
sony-jun <warrise@naver.com>
2022-08-05 17:18:43 +0900

    1회차_손희준_괄호짝짓기_제외_클리어

--- 1회차/손희준/1_조교의성적매기기.py
+++ 1회차/손희준/1_조교의성적매기기.py
@@ -1,3 +1,24 @@
-import sys
+num = int(input())
+per =  ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+for i in range(1,num+1):
+    n,k = map(int,input().split())
+    li = []
+    for j in range(n):
+        mid,fin,res = map(int, input().split())
+        total = (mid*0.35) + (fin*0.45)+(res*0.2)
+        li.append(total)
+    
+    sco = li[k-1]
 
-sys.stdin = open("_조교의성적매기기.txt")
+    li.sort(reverse=True)
+    dig = n//10
+    final = li.index(sco)//dig
+    per_ = per[final]
+
+    print(f'#{i} {per_}')
+
+
+
+
+
+
--- 1회차/손희준/2_암호생성기.py
+++ 1회차/손희준/2_암호생성기.py
@@ -1,3 +1,17 @@
-import sys
+n = 10
+for t in range(1, n+1):
+    tc = int(input())
+    queue = list(map(int, input().split()))
 
-sys.stdin = open("_암호생성기.txt")
+    i = 1
+    while True:
+        if i > 5:
+            i = 1
+        m = queue.pop(0) - i
+        if m <= 0:
+            queue.append(0)
+            break
+        queue.append(m)
+        i += 1
+    print(f'#{t}', end=' ')
+    print(*queue)
