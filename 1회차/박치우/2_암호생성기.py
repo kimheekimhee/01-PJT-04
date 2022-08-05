1bb8df98aa48436fbfb46be70db7b8a3834873d9
chiwoogood <chiwooooo@naver.com>
2022-08-05 17:07:39 +0900

    project04

--- 1회차/박치우/2_암호생성기.py
+++ 1회차/박치우/2_암호생성기.py
@@ -1,3 +1,19 @@
 import sys
+from collections import deque
 
 sys.stdin = open("_암호생성기.txt")
+
+for s in range(10):
+    n = int(input())
+    number = list(map(int,input().split()))
+    number = deque(number)
+    while True:
+        for i in range(len(number)):
+            number[0] -= i
+            number.append(number.popleft)    
+            if number[-1] == 0:
+                s = 'stop'
+                print('#{}'.format(n),number)
+                break
+        if s =='stop':
+            break
--- 1회차/박치우/3_파리퇴치.py
+++ 1회차/박치우/3_파리퇴치.py
@@ -1,3 +1,10 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+test_case = int(input())
+for _ in range(test_case):
+    N, M = map(int,input().split())
+    lst = [list(map(int,input().split())) for _ in range(N)]
+    
+    
