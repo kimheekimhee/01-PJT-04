552bb9d4e650c47ee59c921eb5673060270a9283
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2022-08-05 15:25:07 +0900

    commit

--- 1회차/노가빈/3_파리퇴치.py
+++ 1회차/노가빈/3_파리퇴치.py
@@ -1,3 +1,13 @@
-import sys
+# tc = int(input())
+# for x in range(tc):
+n,m = map(int,input().split(' '))
+nlist = []
+for i in range(n):
+    nlist.append(list(map(int,input().split(' '))))
 
-sys.stdin = open("_파리퇴치.txt")
+numlist = []
+for i in range(n-m+1):
+    for j in range(n-m+1):
+        num = nlist[j][i]+nlist[j][i+1]+nlist[j+1][i]+nlist[j+1][i+1]
+        numlist.append(num)
+print(f'#{1}',max(numlist))
--- 1회차/노가빈/5_괄호짝짓기.py
+++ 1회차/노가빈/5_괄호짝짓기.py
@@ -1,3 +1,30 @@
-import sys
+from ast import expr_context
 
-sys.stdin = open("_괄호짝짓기.txt")
+
+num = int(input())
+strlist = list(input())
+i =0
+while True:
+    try:
+        temp = strlist[i]
+        if temp == '(':
+            strlist.remove('(')
+            strlist.remove(')')
+        elif temp == '{':
+            strlist.remove('{')
+            strlist.remove('}')
+        elif temp == '[':
+            strlist.remove('[')
+            strlist.remove(']')
+        elif temp == '<':
+            strlist.remove('<')
+            strlist.remove('>')
+        i += 1
+        if i == len(strlist)-1:
+            print(f'i는 {i}')
+            print(f'strlist의 길이는 {len(strlist)}')
+            i = 0
+    except ValueError as e:
+        print(strlist)
+        print(e)
+        break
