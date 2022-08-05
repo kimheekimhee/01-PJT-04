df65ab7f5b311c68d9a409dda8e8c715ee74b167
ericaforcoding <ericaforcoding@gmail.com>
2022-08-05 17:22:12 +0900

    0805

--- 1회차/오진수/0_민석이의과제체크하기.py
+++ 1회차/오진수/0_민석이의과제체크하기.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+
+t = int(input())
+
+for tc in range(1, t + 1) :
+    total_, submit_ = map(int, input().split())
+    num_ = list(map(int, input().split()))
+    list_ = []
+
+    for i in range(1, total_+ 1) : #range(total)
+        if i not in num_ :
+            list_.append(i)
+    
+    print(f'#{tc}', *list_) #들여쓰기?
