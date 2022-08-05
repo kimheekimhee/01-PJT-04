e2cbddccd50da1f9390771c2afe943815b59d628
sebin kwon <sbkwon98@gmail.com>
2022-08-05 17:24:05 +0900

    2회차 권세빈 0805 모의고사

--- 2회차/권세빈/0_민석이의과제체크하기.py
+++ 2회차/권세빈/0_민석이의과제체크하기.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T = int(input())
+for t in range(1, T+1):
+    n, k = map(int,input().split())
+    none = []
+    k_list = list(map(int,input().split()))
+    for p in range(1,n+1):
+        if p not in k_list:
+            none.append(p)
+
+    print(f'#{t}',*none)
--- 2회차/권세빈/1_조교의성적매기기.py
+++ 2회차/권세빈/1_조교의성적매기기.py
@@ -1,3 +1,21 @@
 import sys
 
 sys.stdin = open("_조교의성적매기기.txt")
+
+T = int(input())
+grade = ['A+','A0','A-','B+','B0','B-','C+','C0','C-','D0']
+
+for t in range(1, T+1):
+    n, k = map(int,input().split())
+    total = []
+
+    for stu in range(n):
+        mid, final, ass = map(int,input().split())
+        total.append((mid*35/100)+(final*45/100)+(ass*20/100))
+
+        s_total = sorted(total,reverse=True)
+
+    pct = n//10
+    k_index = s_total.index(total[k-1])
+
+    print(f'#{t}',grade[k_index//pct])
