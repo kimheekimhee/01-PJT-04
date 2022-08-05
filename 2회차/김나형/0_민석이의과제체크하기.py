a4ce7dafbc6186fb089ccb7c27261bb0e01a91a0
na_hyeong <skgud9511@gmail.com>
2022-08-05 17:22:05 +0900

    2회차_김나형_220805

--- 2회차/김나형/0_민석이의과제체크하기.py
+++ 2회차/김나형/0_민석이의과제체크하기.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T = int(input())
+
+for i in range(1, T+1):
+    students, pass_ = map(int, input().split())
+    pass_li = list(map(int, input().split()))
+    fail_li = []
+    for student in range(1, students + 1):
+        if student in pass_li:
+            continue
+        else:
+            fail_li.append(str(student))
+    fail = ' '.join(fail_li)
+    print(f'#{i} {fail}')
