e653a4c8b63b437e6aefc91407141d1594ed7de2
buzzzlight <pomodoro5292@gmail.com>
2022-08-05 17:22:27 +0900

    0805

--- 2회차/김수빈/3_파리퇴치.py
+++ 2회차/김수빈/3_파리퇴치.py
@@ -1,3 +1,14 @@
 import sys
-
 sys.stdin = open("_파리퇴치.txt")
+
+T = int(input())
+
+for _ in range(1, T + 1):
+    N, M = map(int, input().split())
+    fly = []
+    for i in range(N):
+        fly.append(list(map(int, input().split())))
+        score = []
+        for j in range(M):
+            score.append(fly[i][j])
+            i += 1
--- 2회차/김수빈/4_어디에단어가들어갈수있을까.py
+++ 2회차/김수빈/4_어디에단어가들어갈수있을까.py
@@ -1,3 +1,12 @@
 import sys
-
 sys.stdin = open("_어디에단어가들어갈수있을까.txt")
+
+T = int(input())
+
+for _ in range(1, T + 1):
+    N, K = map(int, input().spilt())
+    puzzle = []
+    for i in range(N):
+        puzzle.append(list(map(int, input())))
+
+        print(f'#{T}')
