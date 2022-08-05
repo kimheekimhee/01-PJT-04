1ad65b652f32ac927bf73f5c36149ae80ecd58ea
tmsnvl95 <tmsnvl95@naver.com>
2022-08-05 17:22:03 +0900

    1번까지..완료

--- 1회차/이종은/0_민석이의과제체크하기.py
+++ 1회차/이종은/0_민석이의과제체크하기.py
@@ -1,3 +1,32 @@
 import sys
 
-sys.stdin = open("_민석이의과제체크하기.txt")
+sys.stdin = open("sample_input.txt")
+
+ # [1,2,3,4,5]
+list1 = []
+list2 = []
+
+t = int(input()) # tc 받음
+
+for tc in range(1, t+1): # 1~t 기간 횟수까지 반복
+    n, m = map(int, input().split()) # 5, 3 입력
+
+    a = list(map(int, input().split())) # 2, 5, 3 입력
+
+    result = [] # 결과 값 담을 빈 리스트 생성
+
+    for i in range(1, n+1): # 1~5 반복
+        if i not in a: # 1이 a 리스트에 포함되어있지 않다면 
+            result.append(i) # 결과 리스트에 출력 
+
+
+    
+    print(f"#{tc}", *result)
+
+    
+
+
+
+
+
+
--- 1회차/이종은/1_조교의성적매기기.py
+++ 1회차/이종은/1_조교의성적매기기.py
@@ -1,3 +1,40 @@
 import sys
 
-sys.stdin = open("_조교의성적매기기.txt")
+sys.stdin = open("input (3).txt")
+
+t = int(input())
+
+grades = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+
+
+for i in range(1, t+1):
+    n, m = map(int, input().split())
+    result = []
+    for j in range(n):
+        a, b, c = map(int, input().split())
+        sum1 = a*0.35 + b*0.45 + c*0.2
+        result.append(sum1)
+    
+    k_score = result[m-1] #k번째 학생의 인덱스 값 구하기 => 그 학생의 점수
+
+    result1 = sorted(result, reverse=True) #내림차순으로 정렬
+
+    div = n//10 # 동일한 평점 부여하기 때문에 10 나눔
+
+    
+    final = result1.index(k_score) // div # 학생 점수가 있는 인덱스를 번호 추출, 그 번호를 학생수로 나누고
+    # 예를들어 30명일 경우, 3명씩 같은 등급을 받을 수 있음, k가 3인데, 3번재 학생의 점수가 k-score이고, 그 점수의 인덱스 번호를 구함
+    # 근데 인덱스 번호가 3이면 3을 나누기 ? 
+
+    
+
+
+    print(f'#{i}', grades[final]) # grades 리스트에 그 번호 맞는 값 출력 
+
+
+
+        
+
+
+
+
