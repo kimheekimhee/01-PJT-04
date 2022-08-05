0bb26139f5dcf04f203abe9bf0c28ea4fef73d01
ForLunch01 <crazylunch2019@gmail.com>
2022-08-05 15:51:22 +0900

    중간2

--- 3회차/남중식/0_민석이의과제체크하기.py
+++ 3회차/남중식/0_민석이의과제체크하기.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+
+T = int(input())
+
+for i in range(T):
+    N, K = map(int, input().split())
+    N_list = list(map(int, input().split()))
+    
+    print(f'#{i+1}', end=" ")
+    for j in range(1, N+1):
+        if j not in N_list:
+            print(j, end=" ")
+    print()
+    
--- 3회차/남중식/5_괄호짝짓기.py
+++ 3회차/남중식/5_괄호짝짓기.py
@@ -1,3 +1,33 @@
 import sys
 
 sys.stdin = open("_괄호짝짓기.txt")
+
+
+T = 10
+
+for i in range(T):
+    word_len = int(input())
+    word = input()
+    
+    word_stack = []
+    
+    for w in word:
+        word_stack.append(w)
+        if w == ')' and '(' in word_stack:
+            word_stack.pop()
+            word_stack.remove('(')
+        if w == ']' and '[' in word_stack:
+            word_stack.pop()
+            word_stack.remove('[')
+        if w == '>' and '<' in word_stack:
+            word_stack.pop()
+            word_stack.remove('<')
+        if w == '}' and '{' in word_stack:
+            word_stack.pop()
+            word_stack.remove('{')
+            
+    if len(word_stack):
+        print(f'#{i+1}', '0')
+    else:
+        print(f'#{i+1}', '1')
+    
