4f7c9ea6b42d1247000351440361ebd3d76bfb44
hong2HSH <tnghd1717@gmail.com>
2022-08-05 17:21:27 +0900

    4,5남음

--- 3회차/황수홍/0_민석이의과제체크하기.py
+++ 3회차/황수홍/0_민석이의과제체크하기.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T = int(input())
+
+for i in range(T):
+    N, K = map(int,input().split())
+    n = list(map(int,input().split()))
+    res = []
+    for j in range(1,N+1):        
+        if j not in n:
+            res.append(j)
+    print(f'#{i+1}', *res)
--- 3회차/황수홍/3_파리퇴치.py
+++ 3회차/황수홍/3_파리퇴치.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+input = sys.stdin.readline
+T = int(input())
+
+for i in range(T): # 테스트 갯수
+    N, M = map(int,input().split()) # NxN행렬, 파리채크기
+    matrix = [list(map(int,input().split())) for _ in range(N)] # 배열 받기
+    SUM = [] # 모든 경우 기록
+    for j in range(N-(M-1)): # 구간 !
+        for k in range(N-(M-1)):
+            sum = 0
+            for ii in range(M):
+                for jj in range(M):
+                    sum += matrix[j+ii][k+jj]
+            SUM.append(sum)
+    print(f'#{i+1} {max(SUM)}')
