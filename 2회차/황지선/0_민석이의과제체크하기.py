2f9c6d1a65d3dd9ceb2f64f8892f82087436424a
yangu1455 <kunja266@hanmail.net>
2022-08-05 17:27:54 +0900

    220805 모의고사풀이 및 후기

--- 2회차/황지선/0_민석이의과제체크하기.py
+++ 2회차/황지선/0_민석이의과제체크하기.py
@@ -1,3 +1,27 @@
-import sys
+# 민석이는 제출한 사람의 목록을 받았다.
+# 수강생들은 1번에서 N번까지 번호가 매겨져 있고, 어떤 번호의 사람이 제출했는지에 대한 목록을 받은 것이다.
+# 과제를 제출하지 않은 사람의 번호를 오름차순으로 출력하는 프로그램을 작성하라.
 
-sys.stdin = open("_민석이의과제체크하기.txt")
+
+# [출력]
+# 각 테스트 케이스마다 과제를 제출하지 않은 사람의 번호를 오름차순으로 출력한다.
+
+
+# 첫 번째 줄에 테스트 케이스의 수 T가 주어진다.
+T = int(input())
+
+for t in range(1, T+1):
+    # 수강생의 수 N, 과제를 제출한 사람의 수 K가 공백으로 구분되어~
+    N, K = map(int, input().split())
+
+    # 두 번째 줄에는 과제를 제출한 사람의 번호 K개가 공백으로 구분되어~
+    # 번호는 1이상 N이하의 정수이며 같은 번호가 두 번 이상 주어지는 경우는 없다.
+    submission_n = list(map(int, input().split()))
+    res = []
+
+    for n in range(1, N+1):
+        if n not in submission_n:
+            res.append(n)
+
+    res.sort()
+    print(f'#{t}', *res)
--- 2회차/황지선/1_조교의성적매기기.py
+++ 2회차/황지선/1_조교의성적매기기.py
@@ -1,3 +1,34 @@
-import sys
+# 테스트 케이스 두 번째 줄 부터 각각의 학생이 받은 시험 및 과제 점수가 주어진다.
+# 총점 = 중간 35% + 기말 45% + 과제 20%
 
-sys.stdin = open("_조교의성적매기기.txt")
+import copy
+
+dict = {
+    0: 'A+', 1: 'A0', 2: 'A-', 3: 'B+', 4: 'B0', 
+    5: 'B-', 6: 'C+', 7: 'C0', 8: 'C-', 9: 'D0'
+    }
+
+# 입력은 첫 줄에 총 테스트 케이스의 개수 T가 온다.
+T = int(input())
+
+for t in range(1, T+1):
+    # 테스트 케이스의 첫 번째 줄은 학생수 N 과, 학점을 알고싶은 학생의 번호 K 가 주어진다.
+    N, K = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+
+    for x in range(N):
+        # 총점 리스트(등수 안 먹인 것!)
+        matrix[x] = matrix[x][0] * 0.35 + matrix[x][1] * 0.45 + matrix[x][2] * 0.2
+
+    # 총점 리스트 만들어서 소트한 다음에 저장해두고
+    sum_list = []
+    sum_list = sorted(copy.deepcopy(matrix))
+    sum_list.reverse()
+
+    # sum_list로 가서 총점 넣고 해당 인덱스 불러오고
+    # 인덱스에 공식 만들어서 대입해야하는데 머리가 안돌아가서 계산을 못하겠다..!
+    res_index = sum_list.index(matrix[K-1])
+
+    print(f'#{t} {dict[res_index]}')
+
+# 여기까지는 10명일때만 가능함!
