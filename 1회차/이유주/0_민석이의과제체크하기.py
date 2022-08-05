aaba6d55648e53e78d9936b6dbfe2c1213160d4e
rkskakdlfem3 <rkskakdlfem3@gmail.com>
2022-08-05 17:23:23 +0900

    0805 이유주 모의고사

--- 1회차/이유주/0_민석이의과제체크하기.py
+++ 1회차/이유주/0_민석이의과제체크하기.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+N = int(input())
+for i in range(1, N + 1):
+    stu1, sub = map(int, input().split())
+    sub_num = list(map(int, input().split()))
+    
+    list1 = []
+    for ele in range(1, stu1+1):
+        if ele not in sub_num:
+            list1.append(ele)
+    print(f'#{i}', *list1)        
