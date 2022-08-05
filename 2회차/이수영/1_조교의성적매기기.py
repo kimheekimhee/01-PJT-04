62af5856cd9e253b4e548b5ec6de40135485b702
suyoung049 <suyoung049@gmail.com>
2022-08-05 17:38:04 +0900

    2회차 코딩테스트 업로드

--- 2회차/이수영/1_조교의성적매기기.py
+++ 2회차/이수영/1_조교의성적매기기.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_조교의성적매기기.txt")
+
+T = int(input())
+
+N, M = map(int, input().split())
+score_list =[]
+for _ in range(N):
+    a, b, c = map(int, input().split())
+    score = (a * 0.35) + (b * 0.45) + (c * 0.2)
+    score_list.append(score)
+    sor_score_list = sorted(score_list)
+for i in range(len(sor_score_list)):
+    if i < N//10:
+        score_list.replace(sor_score_list[i], 'D0')
+print(score_list)
--- 2회차/이수영/2_암호생성기.py
+++ 2회차/이수영/2_암호생성기.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_암호생성기.txt")
+
+N = int(input())
+code = list(map(int, input().split()))
+
+for j in range(1, 6):       
+        code.append(code.pop(0)-int(j))
+       
+
+
+    
+
+    
+        
+       
