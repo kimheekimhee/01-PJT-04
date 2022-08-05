33bb0623449fd4d31f2d1ac169bbc0c5312a1893
kklee0930 <kklee0930@gmail.com>
2022-08-05 17:23:23 +0900

    220805 1기_3회차_이동희

--- 3회차/이동희/3_파리퇴치.py
+++ 3회차/이동희/3_파리퇴치.py
@@ -1,3 +1,13 @@
-import sys
+T = int(input())
 
-sys.stdin = open("_파리퇴치.txt")
+N, M = map(int,input().split())
+
+matrix = [list(map(int, input().split())) for _ in range(N)]
+
+result = 0
+for i in range(N): # 0,1,2,3,4
+    # 시작점에서 파리채의 끝부분이 전체 가로길이를 넘지 않아야한다.
+    while i + M-1  < N-1:
+        for j in range(N):
+            while j + M < N:
+                result += sum(matrix[i][j:j+M])
--- 3회차/이동희/5_괄호짝짓기.py
+++ 3회차/이동희/5_괄호짝짓기.py
@@ -1,3 +1,28 @@
-import sys
-
-sys.stdin = open("_괄호짝짓기.txt")
+# 머리가 안돌아가네요 멘탈도 박살
+for tc in range(1, 11):
+    length = int(input())
+    str_ = input()
+    for i in range(length):
+        try:
+            if str_[i] == '(' and ')' in str_[i:]:
+                str_ = str_.replace(str_[i], '')
+                str_ = str_[i:].replace(')','')
+                
+            elif str_[i] == '[' and ']' in str_[i:]:
+                str_ = str_.replace(str_[i], '')
+                str_ = str_[i:].replace(']','')
+                
+            elif str_[i] == '{' and '}' in str_[i:]:
+                str_ = str_.replace(str_[i], '')
+                str_ = str_[i:].replace('}','')
+                
+            elif str_[i] == '<' and '>' in str_[i:]:
+                str_ = str_.replace(str_[i], '')
+                str_ = str_[i:].replace('>','')
+        except:
+            break
+            
+    if len(str_) == 0:
+        print(f'#{tc} 1')
+    else:
+        print(f'#{tc} 0')
