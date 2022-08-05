9506d2b607f1321ec458c0338ef186397efb276f
mechauk418 <mechauk418@gmail.com>
2022-08-05 15:21:49 +0900

    0805 파이썬 모의고사

--- 1회차/이진욱/2_암호생성기.py
+++ 1회차/이진욱/2_암호생성기.py
@@ -1,3 +1,27 @@
 import sys
 
 sys.stdin = open("_암호생성기.txt")
+
+
+for i in range(10):
+
+    t = int(input())
+    N_list = list(map(int,input().split()))
+    start = 1
+    while 0 not in N_list:
+        if start >5:
+            start = 1
+        N_list.reverse()
+        num = N_list.pop()
+        N_list.reverse()
+        if (num-start) <= 0:
+            N_list.append(0)
+            print(f'#{t} {" ".join(map(str,N_list))} ')
+            break
+        N_list.append(num-start)
+        start+=1
+
+
+
+
+
--- 1회차/이진욱/3_파리퇴치.py
+++ 1회차/이진욱/3_파리퇴치.py
@@ -1,3 +1,25 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+T=int(input())
+
+for k in range(T):
+
+    N,M=map(int,input().split())
+    N_List = [list(map(int, input().split())) for _ in range(N)]
+
+    sum=0
+    sum_list=[]
+    for a in range(N-(M-1)):
+        for b in range(N-(M-1)):
+            for c in range(a,a+M):
+                for d in range(b,b+M):
+                    sum += N_List[c][d]
+
+            sum_list.append(sum)
+            sum=0
+
+    max_num = max(sum_list)
+
+    print(f'#{k+1} {max_num}')
