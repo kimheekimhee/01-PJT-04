b4a6282d0164548af400bfd4531bfbd2cbd15b5f
psun0610 <psy010610@naver.com>
2022-08-05 16:30:10 +0900

    3회차 박선영 업로드

--- 3회차/박선영/2_암호생성기.py
+++ 3회차/박선영/2_암호생성기.py
@@ -1,3 +1,28 @@
 import sys
 
 sys.stdin = open("_암호생성기.txt")
+
+test_case = 0
+while 1:
+    try:
+        T = int(input())
+    except EOFError:
+        break
+    test_case += 1
+
+    numbers = list(map(int, input().split()))
+    while 1:
+        for i in range(1, 6):               
+            if numbers[0]-i <= 0:             # -1 없이 0을 뒤로 보내고 break
+                numbers.pop(0)                # 큐 이용해서 앞에 빼고 뒤에 붙임
+                numbers.append(0)
+                break
+            else:
+                numbers.append(numbers.pop(0)-i)
+
+        if numbers[-1] == 0:
+            break
+    print(*numbers)
+
+    print(f'#{test_case}', *numbers)
+
--- 3회차/박선영/5_괄호짝짓기.py
+++ 3회차/박선영/5_괄호짝짓기.py
@@ -1,3 +1,50 @@
 import sys
 
 sys.stdin = open("_괄호짝짓기.txt")
+# 노가다 방법 1
+for test_case in range(1, 11):
+    n = int(input())
+    parenth = input()
+    a = []
+    b = []
+    c = []
+    d = []
+    ans = 0
+    for p in parenth:
+        if p == '(':
+            a.append(p)
+        elif p == ')':
+            if len(a) > 0:
+                a.pop()
+            else:
+                ans = 0
+                break
+
+        elif p == '[':
+            b.append(p)
+        elif p == ']':
+            if len(b) > 0:
+                b.pop()
+            else:
+                ans = 0
+                break
+
+        elif p == '{':
+            c.append(p)
+        elif p == '}':
+            if len(c) > 0:
+                c.pop()
+            else:
+                ans = 0
+                break
+        elif p == '<':
+            d.append(p)
+        else:
+            if len(d) > 0:
+                d.pop()
+            else:
+                ans = 0
+                break
+    if len(a) + len(b) + len(c) + len(d) == 0:
+        ans = 1
+    print(f'#{test_case} {ans}')
