b05a4557baef1083078c74c9296a0e4a1e23b2c8
rhoeunbin <rhoeb21@gmail.com>
2022-08-05 17:31:50 +0900

    모의고사 풀이 미완료

--- 2회차/노은빈/3_파리퇴치.py
+++ 2회차/노은빈/3_파리퇴치.py
@@ -1,3 +1,11 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+t = int(input())
+
+for i in range(t):
+    n, m = map(int,input().split())
+    matrix = [list(map(int,input().split())) for _ in range(n)]
+
+    
