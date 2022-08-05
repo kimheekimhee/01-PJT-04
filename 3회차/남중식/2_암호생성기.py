7ef9d7a4217ee08b8a2bdab4c5b3f655e18fb938
ForLunch01 <crazylunch2019@gmail.com>
2022-08-05 16:52:40 +0900

    끝

--- 3회차/남중식/2_암호생성기.py
+++ 3회차/남중식/2_암호생성기.py
@@ -1,3 +1,32 @@
 import sys
 
 sys.stdin = open("_암호생성기.txt")
+
+from collections import deque
+
+def swea_pw_gen():
+
+    for i in range(10):
+        case = int(input())
+        pw_list = list(map(int, input().split()))
+        pw_queue = deque(pw_list)
+        
+        while_checker = True
+        
+        while while_checker == True:
+            for j in range(1,6):
+                pw_queue[0] = pw_queue[0] - j
+                pw_queue.rotate(-1)
+                
+                if pw_queue[-1] <= 0:
+                    pw_queue[-1] = 0
+                    print(f'#{case}',*pw_queue)
+                    while_checker = False
+                    break
+                
+swea_pw_gen()
+            
+    
+            
+        
+        
