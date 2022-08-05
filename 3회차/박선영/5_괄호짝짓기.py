ce4109518708c5d05b9848be5a52ff4a9d4b4678
psun0610 <psy010610@naver.com>
2022-08-05 17:14:57 +0900

    코드추가

--- 3회차/박선영/5_괄호짝짓기.py
+++ 3회차/박선영/5_괄호짝짓기.py
@@ -1,7 +1,9 @@
 import sys
 
 sys.stdin = open("_괄호짝짓기.txt")
-# 노가다 방법 1
+
+# # 노가다 방법 1
+'''
 for test_case in range(1, 11):
     n = int(input())
     parenth = input()
@@ -46,5 +48,31 @@ for test_case in range(1, 11):
                 ans = 0
                 break
     if len(a) + len(b) + len(c) + len(d) == 0:
+        ans = 1
+    print(f'#{test_case} {ans}')
+'''
+
+paren_list = [['(', ')'], ['{', '}'], ['[', ']'], ['<', '>']]
+for test_case in range(1, 11):
+    n = int(input())
+    parenth = input()
+    list_ = [[] for _ in range(4)]
+    ans = 1
+    for p in parenth:
+        for i in range(4):
+            if p == paren_list[i][0]:
+                list_[i].append(paren_list[i][0])
+            elif p == paren_list[i][1]:
+                if len(list_[i]) > 0:
+                    list_[i].pop()
+                else:
+                    ans = 0
+                break
+        if ans == 0:
+            break
+    tmp = 0
+    for i in list_:
+        tmp += len(i)
+    if tmp == 0:
         ans = 1
     print(f'#{test_case} {ans}')
