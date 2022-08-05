2708f8ed94a31f68890966d56f6218a8b745810c
an dong woo <akskwha@naver.com>
2022-08-05 17:57:45 +0900

    220805모의고사풀이

--- 1회차/안동우/0_민석이의과제체크하기.py
+++ 1회차/안동우/0_민석이의과제체크하기.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T=int(input())
+
+for q in range(1,T+1):
+    Z,X=map(int,input().split())#Z학생수 X제출한 수
+    C=list(map(int,input().split()))
+    a=[]
+    for i in range(1,Z+1):
+        if i not in C:
+            a.append(i)
+    print(f'#{q}',*a)
--- 1회차/안동우/1_조교의성적매기기.py
+++ 1회차/안동우/1_조교의성적매기기.py
@@ -1,3 +1,21 @@
 import sys
 
 sys.stdin = open("_조교의성적매기기.txt")
+
+T=int(input())
+s=['A+','A0','A-','B+','B0','B-','C+','C0','C-','D0']
+for q in range(1,T+1):
+    N,K=map(int,input().split())# N학생수 K학점을 알고싶은 학생의 번호
+    e=[]
+    for i in range(N):
+        a,b,c=map(int,input().split())
+        w=(a*0.35)+(b*0.45)+(c*0.2)
+        e.append(w)
+
+
+    r=e[K-1]
+    e.sort(reverse=True)
+    
+    y=N//10
+    u=e.index(r)//y
+    print("#%d %s"% (q,s[u]))
