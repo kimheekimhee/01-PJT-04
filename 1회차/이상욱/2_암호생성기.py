ae533faaae395596c34667fced16670b0aa2b37c
wxxk <dwde22@gmail.com>
2022-08-05 17:55:55 +0900

    암호생성기 추가 풀이

--- 1회차/이상욱/2_암호생성기.py
+++ 1회차/이상욱/2_암호생성기.py
@@ -1,10 +1,16 @@
 import sys
 sys.stdin = open("_암호생성기.txt")
+T = 10
+for t in range(1, T+1):
+    N = int(input())
+    numbers = list(map(int, input().split()))
 
-numbers = [9550, 9556, 9550, 9553, 9558, 9550, 9551, 9551]
-while True:
-    for i in range(1, 6):
-        num = numbers.pop(0)
-        numbers.append(num - i)
 
-print(numbers)
+    while numbers[-1] > 0 :
+        for i in range(1, 6):
+            num = numbers.pop(0)
+            numbers.append(num - i)
+            if numbers[-1] <= 0:
+                numbers[-1] = 0
+                break
+    print(f'#{t}', *numbers)
