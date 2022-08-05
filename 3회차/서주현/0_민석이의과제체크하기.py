6bb9492dbe254d096198c7e3e6075527c3121ca3
sjhmlj <sjhmlj@gmail.com>
2022-08-05 16:02:28 +0900

    3회차 서주현

--- 3회차/서주현/0_민석이의과제체크하기.py
+++ 3회차/서주현/0_민석이의과제체크하기.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T = int(input())
+for t in range(1, T+1) :
+    n, k = list(map(int, input().split()))
+    studentlist = list(map(int, input().split()))
+    nolist = []
+    for i in range(1, n+1) :
+        if i in studentlist :
+            continue
+        else :
+            nolist.append(i)
+
+    print(f'#{t}', end = ' ')
+    print(*nolist)
+
+
