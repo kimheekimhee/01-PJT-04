fd2f56f2932b40f24ba2b11e0d76e26ea3593eee
kmckmc5587 <kmckmc5587@gmail.com>
2022-08-05 16:56:50 +0900

    1기_1회차_김민찬_프로젝트4

--- 1회차/김민찬/4_어디에단어가들어갈수있을까.py
+++ 1회차/김민찬/4_어디에단어가들어갈수있을까.py
@@ -1,3 +1,33 @@
 import sys
 
 sys.stdin = open("_어디에단어가들어갈수있을까.txt")
+
+T = int(input())
+for _ in range(1, T + 1):
+    N, K = map(int, input().split())
+    puzzle = []
+    for i in range(N):
+        puzzle.append(list(map(int, input().split())))
+    # 배열 받기
+    result = 0
+    count = 0
+
+    for j in range(N): # j = 행, k = 열
+        for k in range(N):
+            if puzzle[j][k] == 1:
+                count += 1
+            if puzzle[j][k] == 0 or k == N - 1: # 0을 만났거나 벽을 만났을 때
+                if count == K: # 이전에 계산한 카운트가 K 라면 result에 카운트
+                    result += 1
+                count = 0 # 카운트 초기화
+
+    for k in range(N): # j = 행, k = 열
+        for j in range(N):
+            if puzzle[j][k] == 1: # 1이면 카운트
+                count += 1
+            if puzzle[j][k] == 0 or j == N - 1 :  # 0을 만났거나 벽을 만났을 때
+                if count == K:   # 이전에 계산한 카운트가 k 라면 result에 카운트
+                    result += 1
+                count = 0
+
+    print(f'#{_}', result)
--- 1회차/김민찬/5_괄호짝짓기.py
+++ 1회차/김민찬/5_괄호짝짓기.py
@@ -1,3 +1,5 @@
 import sys
 
 sys.stdin = open("_괄호짝짓기.txt")
+
+# 시간 부족으로 이 문제는 접근을 못 했습니다.
