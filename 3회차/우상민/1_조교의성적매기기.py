951e6e739de74a57c03b29301e5fa383ca14fc00
wsm0409 <wsm0409@naver.com>
2022-08-05 17:23:06 +0900

    Algotest03

--- 3회차/우상민/1_조교의성적매기기.py
+++ 3회차/우상민/1_조교의성적매기기.py
@@ -1,3 +1,45 @@
-import sys
+# import sys
 
-sys.stdin = open("_조교의성적매기기.txt")
+# sys.stdin = open("_조교의성적매기기.txt")
+T = int(input())
+for C in range(1 ,T+1):
+    matrix_ = []
+    N, M = map(int, input().split())
+    rank = []
+    sep_ = N // 10
+    for i in range(N):
+        point = 0
+        matrix_.append(input().split())
+        point += float(matrix_[i][0])*0.35
+        point += float(matrix_[i][1])*0.45
+        point += float(matrix_[i][2])*0.20
+        rank.append(point)
+    answer = sorted(rank)[::-1]
+    Two = sorted(rank)[::-1]
+    count = 0
+    for index in range(10):
+        for idx in range(sep_):
+            if index == 0:
+                answer[count] = 'A+'
+            elif index == 1:
+                answer[count] = 'A0'
+            elif index == 2:
+                answer[count] = 'A-'
+            elif index == 3:
+                answer[count] = 'B+'
+            elif index == 4:
+                answer[count] = 'B0'
+            elif index == 5:
+                answer[count] = 'B-'
+            elif index == 6:
+                answer[count] = 'C+'
+            elif index == 7:
+                answer[count] = 'C0'
+            elif index == 8:
+                answer[count] = 'C-'
+            elif index == 9:
+                answer[count] = 'D0'
+            count += 1
+    for search in range(N):
+        if rank[M-1] == Two[search]:
+            print(f'#{C}', answer[search])  
--- 3회차/우상민/3_파리퇴치.py
+++ 3회차/우상민/3_파리퇴치.py
@@ -1,3 +1,27 @@
-import sys
+#import sys
 
-sys.stdin = open("_파리퇴치.txt")
+#sys.stdin = open("_파리퇴치.txt")
+
+T = int(input())
+for T in range(1, T+1):
+    N, M = map(int,(input().split()))
+    total = 0
+    matrix_ = []
+    for i in range(1, N+1):  #1 ~ 5
+        matrix_.append(list(map(int, (input().split()))))
+    for idx in (range((N-(M-1)))):  # 0 ~ 3
+        for index in (range((N-(M-1)))):   # 0 ~ 3
+            Ch = 0
+            Oh = 0
+            count = 0   
+            for i_ in range((M**2)+M): # 0 ~ 8
+                if Ch == M:
+                    Ch = 0
+                    Oh += 1
+                elif Ch < M:
+                    count += matrix_[idx+Oh][index+Ch]
+                    Ch += 1
+            if count > total:
+                total = count
+    print(f'#{T}', total)
+
