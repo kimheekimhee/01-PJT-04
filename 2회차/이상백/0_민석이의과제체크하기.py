057c37601e69290c2d2ca3c47ae46623e839a8f7
lsb0709 <dltkdqor0709@gmail.com>
2022-08-05 17:22:19 +0900

    0805

--- 2회차/이상백/0_민석이의과제체크하기.py
+++ 2회차/이상백/0_민석이의과제체크하기.py
@@ -1,3 +1,20 @@
-import sys
+# import sys
 
-sys.stdin = open("_민석이의과제체크하기.txt")
+# sys.stdin = open("_민석이의과제체크하기.txt", 'r')
+
+T = int(input())
+
+for test in range(1, T+1):
+
+    N, K = map(int, input().split())
+
+    submit = list(map(int,input().split()))
+
+    matrix = []
+
+    for i in range(1, N+1):
+        # 1 2 3 4 5
+        if i not in submit:
+            matrix.append(i)
+# print(f' #{test} {*matrix}')
+    print('#{}'.format(test), *matrix)
