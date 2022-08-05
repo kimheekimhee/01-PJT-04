23f441a517b0ac9f7ed82a3c7de3dbae798d47f6
minw0095 <kmu6898@naver.com>
2022-08-05 17:27:48 +0900

    20220805_4회차_프로젝트

--- 2회차/강민우/1_조교의성적매기기.py
+++ 2회차/강민우/1_조교의성적매기기.py
@@ -1,3 +1,33 @@
 import sys
 
 sys.stdin = open("_조교의성적매기기.txt")
+
+T = int(input())
+for test_case in range(1, T+1):
+    N, K = map(int, input().split())
+
+    scholar = ['A+','A0','A-','B+','B0','B-','C+','C0','C-','D0']
+    score = []
+    for a in scholar:
+        for b in range(N//10):
+           score.append(a)
+
+    total = []
+    for a in range(N):
+        a,b,c =map(int, input().split())
+        total.append(0.35*a + 0.45*b + 0.2*c)
+    new_total = sorted(total)
+    new_total.reverse()
+
+    result ={}
+    for a in range(N):
+           result[new_total[a]] = result.get(new_total[a],'') + score[a] 
+    
+    print(f'#{test_case} {result.get(total[K-1])}')
+    
+    
+    
+    
+
+
+
--- 2회차/강민우/4_어디에단어가들어갈수있을까.py
+++ 2회차/강민우/4_어디에단어가들어갈수있을까.py
@@ -1,3 +1,41 @@
 import sys
 
 sys.stdin = open("_어디에단어가들어갈수있을까.txt")
+
+T = int(input())
+for test_case in range(1, T+1):
+    N, K = map(int, input().split())
+    puzzle = [list(map(int, input().split())) for _ in range(N)]
+    
+    total = 0
+    for a in range(N):
+        count1 = 1
+        count_list1 = []
+        count2 = 1
+        count_list2 = []
+        for b in range(N-K+1):
+            if puzzle[a][b] == 1:
+                if puzzle[a][b] == puzzle[a][b+1]:
+                    count1 += 1
+                    count_list1.append(count1)
+            if puzzle[a][b] == 0:
+                count1 =0
+                count_list1.append(count1)
+        if max(count_list1) == K:
+            total += count_list1.count(K)
+        
+        for c in range(N-1):
+            if puzzle[c][a] == 1:
+                if puzzle[c][a] == puzzle[c+1][a]:
+                    count2 += 1
+                    count_list2.append(count2)
+            if puzzle[c][a] == 0:
+                count2 =0
+                count_list2.append(count2)
+        if max(count_list2) == K:
+            total += count_list2.count(K) 
+
+    print(total)
+            
+
+
