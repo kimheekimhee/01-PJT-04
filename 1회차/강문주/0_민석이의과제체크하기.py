2e4a9053f1b737f8e20a5cc918f9bb95282bac17
mororong9 <kangmoonju0603@gmail.com>
2022-08-05 17:34:31 +0900

    0805 algorithm test

--- 1회차/강문주/0_민석이의과제체크하기.py
+++ 1회차/강문주/0_민석이의과제체크하기.py
@@ -1,3 +1,18 @@
 import sys
 
 sys.stdin = open("_민석이의과제체크하기.txt")
+
+T = int(input())
+
+
+for test_case in range(1, T + 1):
+    total, present = map(int, input().split())
+    present_list = list(map(int, input().split()))
+    students = []
+
+    for i in range(1, total+1):
+        if i not in present_list:
+            students.append(i)
+
+
+    print(f'#{test_case}',*students)
