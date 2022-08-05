59617a789b26529dc61ee79a7c9ee36238d82815
lol567236 <lol567236@gmail.com>
2022-08-05 17:28:51 +0900

    모의고사

--- 1회차/정효민/3_파리퇴치.py
+++ 1회차/정효민/3_파리퇴치.py
@@ -1,3 +1,19 @@
+from pprint import pprint
 import sys
 
+
 sys.stdin = open("_파리퇴치.txt")
+T = int(input())
+for test_case in range(1, T + 1):
+    a = 0
+    main_mat = []
+    n , m = map(int , input().split())
+    for _ in range(n):
+        line = list(map(int, input().split()))
+        main_mat.append(line)
+    for x in range(m):
+        for y in range(m):
+        
+            print(main_mat[x][y] ,end ='')
+              
+                
