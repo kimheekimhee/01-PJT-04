a1441e6a6afaf5ea87f5659faad02a2836e6bb8e
9776mk <9776mk@gmail.com>
2022-08-05 17:07:35 +0900

    주석 수정

--- 1회차/문경욱/3_파리퇴치.py
+++ 1회차/문경욱/3_파리퇴치.py
@@ -2,24 +2,30 @@
 
 # sys.stdin = open("_파리퇴치.txt")
 
-# T = int(input())
-T = 1
+# 강사님께서 오늘 알려주신 dx, dy를 활용해서 풀고 싶었는데, 오늘 막 배운 방법이라 익숙하지 않아 실수를 할까봐 그냥 for문으로 풀었습니다.
+# for문을 4번이나 중첩하여 시간 복잡도가 O(N^4)인 것을 알고 있는데, 최대 값이 15이기에 15^4를 해도 50,625 번의 연산밖에 일어나지 않아 그 방법으로 풀었습니다.
+# 나중에 dx,dy를 좀 더 공부하고 익힌 다음 그 방법으로 풀면 훨씬 더 빠르고 간편하게 풀 수 있을 것 같습니다.
+
+T = int(input())
+
 for test_case in range(1, T+1):
     N, M = map(int, input().split())
     list_ = [list(map(int, input().split())) for _ in range(N)]
-    # print(list_)
 
-    sum_ = 0
+    # 합을 저장할 리스트를 찾고
     sum_list =[]
-    
-    
-    # 0부터 N-M까지
-    for i in range(N-M+1):
-        # 0부터 N-M까지
-        for j in range(N-M+1):
-            #for k in range(M):
-            sum_ = list_[i][j] + list_[i][j+1] + list_[i+1][j] + list_[i+1][j+1]
-            sum_list.append(sum_)
-    print(sum_list)
 
+    # 파리채의 크기가 M인 경우 list의 범위가 끝까지 가면 index를 넘어가므로
+    # 가로 0부터 N-M까지
+    for i in range(N-M+1):
+        # 세로 0부터 N-M까지 
+        for j in range(N-M+1):
+            sum_ = 0
+            # 파리채의 크기만큼의 값들을 전부 더한 후
+            for k in range(M):
+                for l in range(M):
+                    sum_ += list_[i+k][j+l]
+            # 합을 저장하는 list에 저장
+            sum_list.append(sum_)
+    # 합 중 최댓값 출력
     print(f'#{test_case} {max(sum_list)}')
--- 1회차/문경욱/5_괄호짝짓기.py
+++ 1회차/문경욱/5_괄호짝짓기.py
@@ -1,20 +1,30 @@
-import sys
+# import sys
 
-sys.stdin = open("_괄호짝짓기.txt")
+# sys.stdin = open("_괄호짝짓기.txt")
 
+# 딕셔너리를 활용하여 여는 괄호인 경우 해당 키 값의 밸류 +1, 닫는 경우 해당 키 값의 밸류 -1를 하고 
+# 마지막에 모든 value가 0인 경우 1, 아니면 0을 출력하는 방법을 생각해냈습니다.
+# 위의 방법이 더 간단할 것 같았지만
+# 이번주에 배운 스택을 활용하려다 보니 조건이 너무 많아진 것 같습니다.
+# 그런데 스택을 활용하기 위한 다른 방법들은 생각나지 않아 다른 분들의 코드를 확인해봐야 할 것 같습니다.
+
+# 10개의 테스트 케이스
 for test_case in range(1, 11):
+    # 값 입력
     N = int(input())
     list_ = list(map(str, input()))
 
-    # print(list_)
-
+    # 유효성 여부를 위한 변수
     is_paried = 1
 
+    # 4종류의 괄호 문자들을 담기 위한 list 생성
     gh_1 = []
     gh_2 = []
     gh_3 = []
     gh_4 = []
 
+    # 여는 괄호일 경우 해당 list에 append
+    # 닫는 괄호일 경우 해당 list에서 빼낼 수 있으면 pop, 빼낼 수 없으면 유효성 여부를 0으로 만든 뒤 break
     for gh in list_:
         if gh == '(':
             gh_1.append(gh)
@@ -49,9 +59,10 @@ for test_case in range(1, 11):
                 is_paried = 0
                 break
 
-    #print(gh_1, gh_2, gh_3, gh_4)        
-
     print(f'#{test_case}', end=' ')
+    # 유효성 변수가 1이고(닫는 기호가 더 많지 않을 때)
+    # 각각의 list들이 비어있다면(여는 기호가 더 많지 않을 때) 1 출력
+    # 둘 중 하나라도 만족하지 않는다면 0 출력
     if is_paried == 1 and gh_1 == [] and gh_2 ==[] and gh_3 ==[] and gh_4 == []:
         print(1)
     else:
