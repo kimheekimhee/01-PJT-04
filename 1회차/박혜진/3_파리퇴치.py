17a2a4017d43cc2d6f53e83ae67e4e0c95fbbf3a
Hye <jelly12paw@gmail.com>
2022-08-05 17:26:24 +0900

    1기 1회차 박혜진 01-PJT-04 문제풀이

--- 1회차/박혜진/3_파리퇴치.py
+++ 1회차/박혜진/3_파리퇴치.py
@@ -1,3 +1,10 @@
-import sys
+# import sys
 
-sys.stdin = open("_파리퇴치.txt")
+# sys.stdin = open("_파리퇴치.txt")
+
+# for num in range(1, int(input()) + 1) :
+n, m = map(int, input().split())
+
+flys = [list(map(int, (input().split()))) for _ in range(n)]
+
+print(flys)
--- 1회차/박혜진/5_괄호짝짓기.py
+++ 1회차/박혜진/5_괄호짝짓기.py
@@ -1,3 +1,49 @@
 import sys
 
 sys.stdin = open("_괄호짝짓기.txt")
+
+for num in range(1, 11) :
+    gualho_len = int(input())
+    gualho = input()
+
+    g = []
+    for _ in range(gualho_len) :
+        for i in gualho :
+            if i == '(' :
+                g.append(i)
+            elif i == ')' :
+                if '(' in g :
+                    g.remove('(')
+                else :
+                    g.append(i)
+
+            if i == '[' :
+                g.append(i)
+            elif i == ']' :
+                if '[' in g :
+                    g.remove('[')
+                else :
+                    g.append(i)
+
+            if i == '{' :
+                g.append(i)
+            elif i == '}' :
+                if '{' in g :
+                    g.remove('{')
+                else :
+                    g.append(i)
+
+            if i == '<' :
+                g.append(i)
+            elif i == '>' :
+                if '<' in g :
+                    g.remove('<')
+                else :
+                    g.append(i)
+
+    if g == [] :
+        print(f'#{num} {1}')
+
+    else :
+        print(f'#{num} {0}')
+            
