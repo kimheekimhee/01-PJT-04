933d7ebf405b495cfd87e1d47cfad40840529fd6
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-08-05 16:09:29 +0900

    0805 모의고사

--- 1회차/김다겸/1_조교의성적매기기.py
+++ 1회차/김다겸/1_조교의성적매기기.py
@@ -1,3 +1,24 @@
-import sys
+# import sys
 
-sys.stdin = open("_조교의성적매기기.txt")
+# sys.stdin = open("_조교의성적매기기.txt")
+
+grade = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
+
+T = int(input())
+for t in range(1, T+1):
+    n, k = map(int, input().split())
+    score = []
+
+    for i in range(n):
+        mid, final, assign = map(int, input().split())
+        score.append(mid * 0.35 + final * 0.45 + assign * 0.2)
+
+    # k의 인덱스 구하기
+    k_score = score[k-1]
+    # 점수를 내림차순으로
+    sorted_score = sorted(score, reverse=True)
+    # k의 인덱스를 정렬한 점수 리스트에서 찾아서 n//10으로 나누기
+    rank = sorted_score.index(k_score) // (n//10)
+    # grade에서 k rank의 인덱스로 정답 찾기
+    k_grade = grade[rank]
+    print(f'#{t} {k_grade}')
--- 1회차/김다겸/3_파리퇴치.py
+++ 1회차/김다겸/3_파리퇴치.py
@@ -1,3 +1,29 @@
-import sys
+# import sys
 
-sys.stdin = open("_파리퇴치.txt")
+# sys.stdin = open("_파리퇴치.txt")
+
+T = int(input())
+
+for t in range(1, T+1):
+    n, m = map(int, input().split())
+
+    # arr 입력받기
+    arr = [list(map(int, input().split())) for _ in range (n)]
+    # mXm에서의 sum 저장할 변수 초기화
+    sum_ = 0
+    # sum_ 값 저장할 리스트 초기화
+    sum_list = []
+
+    # nXn 순회
+    for i in range(n-m+1):
+        for j in range(n-m+1):
+            # mXm 순회
+            for k in range(i, i+m):
+                for l in range(j, j+m):
+                    # sum_에 합 값 
+                    sum_ += arr[k][l]
+            # sum_list에 합 값 추가하기
+            sum_list.append(sum_)
+            # 합 값 초기화
+            sum_ = 0
+    print(f'#{t} {max(sum_list)}')
