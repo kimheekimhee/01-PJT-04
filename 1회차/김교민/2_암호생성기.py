0eaf8866faf168102eae2e3ec2259a8caedad85c
kyominkim1074 <kyominkim1074@gmail.com>
2022-08-05 17:22:18 +0900

    1기_1회차_김교민 모의고사 문제풀이

--- 1회차/김교민/2_암호생성기.py
+++ 1회차/김교민/2_암호생성기.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_암호생성기.txt")
+
+def pass_(p_list): # 좀 더 깔끔하게 진행하기 위해서 함수를 만든다.
+    while True:    
+        for i in range(1, 6): #아래 append까지의 작업을 5번 반복한 것이 1사이클
+            n = p_list.pop(0) 
+            p_list.append(n - i)
+            if p_list[-1] <= 0: #리스트의 맨 끝의 숫자가 0이하인 경우
+                p_list[-1] = 0 # 숫자를 0으로 바꾼다.
+                return p_list
+
+for _ in range(1, 11):
+    t = int(input())
+    p = list(map(int, input().split()))
+    answer = pass_(p)
+    
+    print(f'#{t}', *answer)
--- 1회차/김교민/3_파리퇴치.py
+++ 1회차/김교민/3_파리퇴치.py
@@ -1,3 +1,24 @@
 import sys
 
 sys.stdin = open("_파리퇴치.txt")
+
+t = int(input())
+for case in range(1, t+1):
+    n, m = map(int, input().split())
+    #파리채를 휘두를 수 있는 영역을 2차원 배열로 만든다.
+    board = [list(map(int, input().split())) for _ in range(n)]
+    # 그냥 n인 경우 인덱스 범위에서 벗어나며
+    # n-m의 경우 m의 값이 증가할 수록 이중 for문으로 탐색할 수 있는 범위가 줄어든다.
+    no_flapper = n-m+1
+    flapper = [] #죽은 파리의 수의 합을 저장할 공간
+    
+    #파리채 크기만큼 이중 for문으로 탐색을 한다.
+    for i in range(no_flapper):
+        for j in range(no_flapper):
+            sum_ = 0
+            for di in range(m):
+                for dj in range(m):
+                    sum_ += board[i+di][j+dj]
+            flapper.append(sum_)
+            
+    print(f'#{case} {max((flapper))}')
