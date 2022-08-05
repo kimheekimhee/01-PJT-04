cbd4ed9e0136296050641d1df1a5101dfa8be303
riumr <atrium_grate.0j@icloud.com>
2022-08-05 16:49:57 +0900

    220805 실습

--- 3회차/김태형/2_암호생성기.py
+++ 3회차/김태형/2_암호생성기.py
@@ -1,3 +1,12 @@
-import sys
+# import sys
 
-sys.stdin = open("_암호생성기.txt")
+# sys.stdin = open("_암호생성기.txt")
+
+A = [9550,9556,9550,9553,9558,9551,9551,9551]
+while A:
+    for i in range(1,5):
+        A.append(A[0]-i)
+        A.remove(A[0])
+    if A[7]==0:
+        break
+print(A)
