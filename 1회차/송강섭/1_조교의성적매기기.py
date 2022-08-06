361bd222e410307fb210b1a8a89ce5253365220b
1c0332zz <1c0332zz@gmail.com>
2022-08-07 00:00:25 +0900

    220807

--- 1회차/송강섭/1_조교의성적매기기.py
+++ 1회차/송강섭/1_조교의성적매기기.py
@@ -2,7 +2,10 @@ import sys
 
 sys.stdin = open("_조교의성적매기기.txt")
 
+# 모르겠다
 T = int(input())
+grades = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+
 for test_case in range(1, T+1):
     # n = 학생수, k = 학점확인
     n, k = map(int, input().split())
@@ -11,18 +14,11 @@ for test_case in range(1, T+1):
         a, b, c = map(float, input().split())
         d = (a * 0.35) + (b * 0.45) + (c * 0.2)
         arr.append(d)
-        sorted(arr)
-        cnt = len(arr)
-        num = cnt % 10
-    if cnt == 10:
-        arr[0] = 'A+'
-        arr[1] = 'A0'
-        arr[2] = 'A-'
-        arr[3] = 'B+'
-        arr[4] = 'B0'
-        arr[5] = 'B-'
-        arr[6] = 'C+'
-        arr[7] = 'C0'
-        arr[8] = 'C-'
-        arr[9] = 'D0'
-print(arr[k])
+# print(arr)
+# [74.6, 92.55000000000001, 88.8, 99.45, 72.35, 85.85000000000001, 96.25, 68.95, 85.5, 85.75]
+    # k번째 학생의 인덱스를 구해줘서 성적을 매김
+    k_score = arr[k-1]
+    # 성적을 높은순으로 정렬해줌
+    arr.sort(reverse=True)
+    final_k_score = arr.index(k_score) // (n//10)
+    print(f'#{test_case} {grades[final_k_score]}')
--- 1회차/송강섭/2_암호생성기.py
+++ 1회차/송강섭/2_암호생성기.py
@@ -3,9 +3,8 @@ import sys
 
 sys.stdin = open("_암호생성기.txt")
 
-list_ = [9550, 9556, 9550, 9553, 9558, 9551, 9551, 9551]
-b = []
-for i in list_:
-    b = i - 1
-    list_.append(b)
-print(list_)
+# list_ = [9550, 9556, 9550, 9553, 9558, 9551, 9551, 9551]
+# b = []
+# for i in list_:
+#     b = i - 1
+#     list_.append(b)
