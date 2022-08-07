30a2d13def4571e8db287646f87700b6272a82fc
ererink <yelkim0210@gmail.com>
2022-08-07 16:22:42 +0900

    문제 풀이 추가

--- 2회차/김예린/2_암호생성기.py
+++ 2회차/김예린/2_암호생성기.py
@@ -1,14 +1,19 @@
 import sys
 from collections import deque
 
-test_case = int(input())
-pw = deque(map(int, input().split()))
+for _ in range(10):
+    # 입력
+    test_case = int(input())
+    pw = deque(map(int, input().split()))
 
-while pw[-1] > 0:
-    for i in range(1, 6):
-        pw[0] = pw[0] - i
-        pw.append(pw.popleft())
-        
-print('#{} {}'.format(test_case, pw, end = ' '))
+    while pw[-1] > 0:
+        for i in range(1, 6):
+            pw[0] = pw[0] - i
+            pw.append(pw.popleft())
+            if pw[-1] <= 0:
+                pw[-1] = 0
+                break
+            
+    print(f'#{test_case}', *pw)
 
 sys.stdin = open("_암호생성기.txt")
--- 2회차/김예린/3_파리퇴치.py
+++ 2회차/김예린/3_파리퇴치.py
@@ -5,18 +5,26 @@ for test_case in range(1, t + 1):
     n, m = map(int, input().split())        # n == 배열, m == 파리채 면적 
 
     metrix = []
-    sum_ = 0
-
     for _ in range(n):                      # 배열 만들기
         metrix.append(list(input().split()))
 
+    max_flies = 0
+    
     for i in range(n):
-        for j in range(m):
-            while True: 
-                if j + 1 == n:
-                    sum_ = sum(metrix[i][j] + metrix[i + 1][j] + metrix[j][i] +  metrix[j+1][i])
-
-                    print(sum_)
-                                                # m x m에서 합산값이 제일 큰 값을 출력 == 순회
+        for j in range(n):
+            
+            hit = 0
+            for x in range(j, j + m): 
+                if x > n -1:
+                    break
+                for y in range(i + i +m):
+                    if y > n -1: 
+                        break
+                    hit += int(metrix[x][y])
+                            
+            if max_flies < hit:
+                max_flies = hit
+    
+    print('#{} {}'.format(test_case, max_flies))
 
 sys.stdin = open("_파리퇴치.txt")
