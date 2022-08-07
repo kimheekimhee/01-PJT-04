85d3917db61828b66f7a638b4c32a9ce15e60bcf
na_hyeong <skgud9511@gmail.com>
2022-08-08 00:14:45 +0900

    2회차_김나형_프로젝트_3회(최종)

--- 2회차/김나형/4_어디에단어가들어갈수있을까.py
+++ 2회차/김나형/4_어디에단어가들어갈수있을까.py
@@ -1,3 +1,39 @@
 import sys
 
 sys.stdin = open("_어디에단어가들어갈수있을까.txt")
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    puzzle = [list(map(int, input().split())) for _ in range(N)]
+
+    cnt_r = 0
+    cnt_c = 0
+
+    for r in puzzle:
+        cnt = 0
+        for i in r:
+            if i == 1:
+                cnt += 1
+            else:
+                if cnt == K:
+                    cnt_r += 1   
+                cnt = 0
+        if cnt == K:
+            cnt_r += 1
+            
+            
+    for c in range(N):
+        cnt = 0
+        for r in range(N):
+            if puzzle[r][c] == 1:
+                cnt += 1
+            else:
+                if cnt == K:
+                    cnt_c += 1
+                cnt = 0
+        if cnt == K:
+            cnt_r += 1
+                
+                
+    print(f'#{tc} {cnt_c + cnt_r}')
