c6cdf50da1559cf2472d2abc9f97aa792a96d6b4
MoonSanghee <psycho13th@gmail.com>
2022-08-11 23:02:18 +0900

    모의고사 2회차 피드적용한 코드 추가

--- 2회차/문상희/0_민석이의과제체크하기.py
+++ 2회차/문상희/0_민석이의과제체크하기.py
@@ -7,13 +7,34 @@ test_case = int(input())
 for test in range(1, test_case + 1):
     people, done = map(int, input().split())
     done_list = list(map(int, input().split()))
-    # 전체 수업 들은 사람은 range(1, people + 1)이고 과제를 제출한 사람이 몇명이고 누구인지 정보가 주어져있습니다.
+
     do_not = []
     for i in range(1, people + 1):
         if i not in done_list:
             do_not.append(i)
-            # 그래서 빈 리스트를 만들고 전체를 순회하며 빈 리스트에 낸 사람에 포함되지 않은 사람들을 모아줍니다.
+
     print(f'#{test}', end = ' ')
     for i in do_not:
         print(i, end = ' ')
     print()
+
+
+
+
+
+# 나의 풀이
+# test_case = int(input())
+
+# for test in range(1, test_case + 1):
+#     people, done = map(int, input().split())
+#     done_list = list(map(int, input().split()))
+#     # 전체 수업 들은 사람은 range(1, people + 1)이고 과제를 제출한 사람이 몇명이고 누구인지 정보가 주어져있습니다.
+#     do_not = []
+#     for i in range(1, people + 1):
+#         if i not in done_list:
+#             do_not.append(i)
+#             # 그래서 빈 리스트를 만들고 전체를 순회하며 빈 리스트에 낸 사람에 포함되지 않은 사람들을 모아줍니다.
+#     print(f'#{test}', end = ' ')
+#     for i in do_not:
+#         print(i, end = ' ')
+#     print()
--- 2회차/문상희/5_괄호짝짓기.py
+++ 2회차/문상희/5_괄호짝짓기.py
@@ -2,45 +2,68 @@ import sys
 
 sys.stdin = open("_괄호짝짓기.txt")
 
+# 피드 참고 수정
 for test in range(1, 11):
     num = int(input())
     sen = list(input())
-
+    d = {')': '(', ']': '[', '}': '{', '>': '<'}
     check = []
     for i in sen:
         if i =='(' or i == '[' or i == '{' or i =='<':
             check.append(i)
-            # 열리는 괄호의 경우 아직 나온 상황에서 짝을 확인 안 해도 되니 리스트에 추가하여 줍니다.
-        elif i ==')':
-            if check[-1] != '(':
-                print(f'#{test} 0')
-                break
-            else:
-                check.pop()
-        elif i == ']':
-            if check[-1] != '[':
-                print(f'#{test} 0')
-                break
-            else:
-                check.pop()
-        elif i == '}':
-            if check[-1] != '{':
-                print(f'#{test} 0')
-                break
-            else:
-                check.pop()
-        elif i == '>':
-            if check[-1] != '<':
-                print(f'#{test} 0')
-                break
-            else:
-                check.pop()
-        # 닫힌 괄호의 경우 리스트의 마지막에 저장된 괄호가 본인의 짝인지 확인하고 맞으면 팝하고 아니면 
-        # 식이 성립할 수 없으니 식이 성립할 수 없다고 해주고 브레이크를 걸어줍니다.
+        elif d[i] != check[-1]:
+            print(f'#{test} 0')
+            break
+        else:
+            check.pop()
     else:
         if len(check) !=0:
             print(f'#{test} 0')
-    # 문자열를 다 돌았는데 리스트에 저장된 값이 있으면 닫히지 않은 괄호가 있으니 식이 성립할 수 없습니다.
         else:    
             print(f'#{test} 1')
-    # 이외의 경우는 식이 성립한기에 1을 출력해줍니다.
+
+
+
+# 나의 풀이
+# for test in range(1, 11):
+#     num = int(input())
+#     sen = list(input())
+
+#     check = []
+#     for i in sen:
+#         if i =='(' or i == '[' or i == '{' or i =='<':
+#             check.append(i)
+#             # 열리는 괄호의 경우 아직 나온 상황에서 짝을 확인 안 해도 되니 리스트에 추가하여 줍니다.
+#         elif i ==')':
+#             if check[-1] != '(':
+#                 print(f'#{test} 0')
+#                 break
+#             else:
+#                 check.pop()
+#         elif i == ']':
+#             if check[-1] != '[':
+#                 print(f'#{test} 0')
+#                 break
+#             else:
+#                 check.pop()
+#         elif i == '}':
+#             if check[-1] != '{':
+#                 print(f'#{test} 0')
+#                 break
+#             else:
+#                 check.pop()
+#         elif i == '>':
+#             if check[-1] != '<':
+#                 print(f'#{test} 0')
+#                 break
+#             else:
+#                 check.pop()
+#         # 닫힌 괄호의 경우 리스트의 마지막에 저장된 괄호가 본인의 짝인지 확인하고 맞으면 팝하고 아니면 
+#         # 식이 성립할 수 없으니 식이 성립할 수 없다고 해주고 브레이크를 걸어줍니다.
+#     else:
+#         if len(check) !=0:
+#             print(f'#{test} 0')
+#     # 문자열를 다 돌았는데 리스트에 저장된 값이 있으면 닫히지 않은 괄호가 있으니 식이 성립할 수 없습니다.
+#         else:    
+#             print(f'#{test} 1')
+#     # 이외의 경우는 식이 성립한기에 1을 출력해줍니다.
